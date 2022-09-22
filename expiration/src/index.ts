2b5fa0e8b1e90df8bab1e332eeb0e3184ba9044a
Ibraheem Muhammadawwal <Ibraheemmuhdawwal@gmail.com>
2022-09-22 07:24:24 +0100

    change

--- expiration/src/index.ts
+++ expiration/src/index.ts
@@ -6,7 +6,7 @@ const start = async () => {
     throw new Error('NATS_CLIENT_ID must be defined');
   }
   if (!process.env.NATS_URL) {
-    throw new Error('NATS_URL must be defined...');
+    throw new Error('NATS_URL must be defined..');
   }
   if (!process.env.NATS_CLUSTER_ID) {
     throw new Error('NATS_CLUSTER_ID must be defined');
--- tickets/src/routes/new.ts
+++ tickets/src/routes/new.ts
@@ -1,20 +1,20 @@
-import express, { Request, Response } from "express";
-import { body } from "express-validator";
-import { requireAuth, validateRequest } from "@cygnetops/common-v2";
-import { Ticket } from "../models/ticket";
-import { TicketCreatedPublisher } from "../events/publishers/ticket-created-publisher";
-import { natsWrapper } from "../nats-wrapper";
+import express, { Request, Response } from 'express';
+import { body } from 'express-validator';
+import { requireAuth, validateRequest } from '@cygnetops/common-v2';
+import { Ticket } from '../models/ticket';
+import { TicketCreatedPublisher } from '../events/publishers/ticket-created-publisher';
+import { natsWrapper } from '../nats-wrapper';
 
 const router = express.Router();
 
 router.post(
-  "/api/tickets",
+  '/api/tickets',
   requireAuth,
   [
-    body("title").not().isEmpty().withMessage("Title is required"),
-    body("price")
+    body('title').not().isEmpty().withMessage('Title is required.'),
+    body('price')
       .isFloat({ gt: 0 })
-      .withMessage("Price must be greater than 0"),
+      .withMessage('Price must be greater than 0'),
   ],
   validateRequest,
   async (req: Request, res: Response) => {
